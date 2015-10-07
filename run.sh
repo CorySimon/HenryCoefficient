414ab44d71654437217fe8161e9b2cae0c7922d2
CorySimon <corysimon@berkeley.edu>
2015-10-07 13:12:04 -0700

    uncomment

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ###
 ### Benchmark CUDA and OpenMP code performance
 ###
- # echo "GPUkernelcalls,time" > GPU_performance.csv
+echo "GPUkernelcalls,time" > GPU_performance.csv
 echo "EquivalentGPUkernelcalls,time" > OpenMP_performance.csv
     
 export OMP_NUM_THREADS=24
@@ -17,9 +17,9 @@ do
     ###
     # GPU code
     ###
- #     t=$({ time ./henry $n >/dev/null; } |& grep real | awk '{print $2}')
- #     echo -e "\tCUDA run time: $t"
- #     echo "$n,$t" >> GPU_performance.csv  # write results to .csv
+    t=$({ time ./henry $n >/dev/null; } |& grep real | awk '{print $2}')
+    echo -e "\tCUDA run time: $t"
+    echo "$n,$t" >> GPU_performance.csv  # write results to .csv
     
     ###
     # OpenMP code
