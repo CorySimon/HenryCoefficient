c47ae84b840106e3bb8bd33962c8b519426ddde9
Jeff Hammond <jeff.science@gmail.com>
2015-10-28 06:44:54 -0700

    change run script to only do CPU

--- run.sh
+++ run.sh
@@ -3,28 +3,28 @@
 ###
 ### Benchmark CUDA and OpenMP code performance
 ###
-echo "GPUkernelcalls,time" > GPU_performance.csv
+#echo "GPUkernelcalls,time" > GPU_performance.csv
 echo "EquivalentGPUkernelcalls,time" > OpenMP_performance.csv
-    
-export OMP_NUM_THREADS=24
-echo "Running with $OMP_NUM_THREADS OpenMP threads"
+
+#export OMP_NUM_THREADS=24
+#echo "Running with $OMP_NUM_THREADS OpenMP threads"
 
 # Run codes with varying numbers of Monte Carlo insertions, $n
 for n in `seq 1 10 500`
 do
-    echo "Running with $n GPU kernel calls"
-    
+    #echo "Running with $n GPU kernel calls"
+
     ###
     # GPU code
     ###
-    t=$({ time ./henry $n >/dev/null; } |& grep real | awk '{print $2}')
-    echo -e "\tCUDA run time: $t"
-    echo "$n,$t" >> GPU_performance.csv  # write results to .csv
-    
+    #t=$({ time ./henry_gpu $n >/dev/null; } |& grep real | awk '{print $2}')
+    #echo -e "\tCUDA run time: $t"
+    #echo "$n,$t" >> GPU_performance.csv  # write results to .csv
+
     ###
     # OpenMP code
     ###
-    t=$({ time ./henry_serial $n >/dev/null; } |& grep real | awk '{print $2}')
+    t=$({ time ./henry_cpu $n >/dev/null; } |& grep real | awk '{print $2}')
     echo -e "\tOpenMP run time: $t"
     echo "$n,$t" >> OpenMP_performance.csv  # write results to .csv
 
